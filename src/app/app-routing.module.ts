fe0181c135fc41fc5bcf711dd03a2477379f8999
Atif <mirzaexpert786@gmail.com>
2023-03-26 14:11:16 +0530

    First Commit to Repository

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NavigationComponent } from './components/header/navigation/navigation.component';
+import { SearchbarComponent } from './components/header/searchbar/searchbar.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    component:SearchbarComponent
+  },{
+    path:'',
+    component:NavigationComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SearchbarComponent } from './components/header/searchbar/searchbar.component';
+import { NavigationComponent } from './components/header/navigation/navigation.component';
+import { HomeComponent } from './components/home/home.component';
+import { NewsComponent } from './components/news/news.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchbarComponent,
+    NavigationComponent,
+    HomeComponent,
+    NewsComponent,
   ],
   imports: [
     BrowserModule,
